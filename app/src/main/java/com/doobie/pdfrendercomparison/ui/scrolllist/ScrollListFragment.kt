c3a920917f314df57ed54cdc294e0d9a5985cc69
Alex Doub <alexdoub1990@gmail.com>
2020-04-09 12:47:54 -0700

    got zoom kinda workin

--- app/src/main/java/com/doobie/pdfrendercomparison/ui/scrolllist/ScrollListFragment.kt
+++ app/src/main/java/com/doobie/pdfrendercomparison/ui/scrolllist/ScrollListFragment.kt
@@ -1,6 +1,5 @@
 package com.doobie.pdfrendercomparison.ui.scrolllist
 
-import android.graphics.PointF
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -17,6 +16,10 @@ class ScrollListFragment : Fragment() {
 
     lateinit var binding: FragmentScrollListBinding
 
+    class ZoomOperation(val page: Int, val xPercent: Float, val yPercent: Float, val scale: Float)
+
+    var queuedZoom : ZoomOperation? = null
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -28,80 +31,58 @@ class ScrollListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        lifecycleScope.launch {
+        val somePageNumber = 82 // zero indexed
+        binding.jumpButton.setOnClickListener {
+            val xPercent = .5f
+            val yPercent = .5f
+            val zoomScale = 2.0f
 
-            val file = getPdfFile(requireContext())
-
-            binding.pdfView.fromFile(file)
-                .pageSnap(true) // snap pages to screen boundaries
-                .pageFling(false) // make a fling change only a single page like ViewPager
-                .nightMode(false) // toggle night mode
-                .enableAntialiasing(true) // improve rendering a little bit on low-res screens
-                .enableSwipe(true) // allows to block changing pages using swipe
-                .swipeHorizontal(false)
-                .enableDoubletap(true)
-                .pageFitPolicy(FitPolicy.HEIGHT)
-                .defaultPage(0)
-                .onRender {
-                    testScrollAndZoomToSpecificPlace()
-                }
-                .onPageScroll { page, positionOffset ->
-
-                    println("@@ did scroll to page $page with TOTAL offset $positionOffset%")
-                }
-                .spacing(8)
-                .enableAnnotationRendering(true)
-                .load()
+            jumpTo(somePageNumber, xPercent, yPercent, zoomScale)
         }
+        binding.jumpButton.text = "Jump to page ${somePageNumber + 1}"
+
+        binding.pdfView.fromFile(getPdfFile(requireContext()))
+            .pageSnap(true) // snap pages to screen boundaries
+            .pageFling(false) // make a fling change only a single page like ViewPager
+            .nightMode(false) // toggle night mode
+            .enableAntialiasing(true) // improve rendering a little bit on low-res screens
+            .enableSwipe(true) // allows to block changing pages using swipe
+            .swipeHorizontal(false)
+            .enableDoubletap(true)
+            .pageFitPolicy(FitPolicy.HEIGHT)
+            .defaultPage(0)
+            .onRender {
+                binding.jumpButton.isEnabled = true
+            }
+            .onPageScroll { page, positionOffset ->
+
+                // So hacky... but we cant change page & zoom at the same time. we must wait for the page to load
+                if (page == queuedZoom?.page) {
+                    val zoomToDo = queuedZoom!!
+                    queuedZoom = null
+
+                    lifecycleScope.launch {
+                        delay(500)  //eww... but we must wait for the page to ACTUALLY load
+                        val xOffset = zoomToDo.xPercent * binding.pdfView.width
+                        val yOffset = zoomToDo.yPercent * binding.pdfView.height
+                        binding.pdfView.zoomWithAnimation(xOffset, yOffset, zoomToDo.scale)
+                    }
+                }
+            }
+            .onLoad { page ->
+
+            }
+            .spacing(8)
+            .enableAnnotationRendering(true)
+            .load()
     }
 
-    private fun testScrollAndZoomToSpecificPlace() {
-        // TEST INPUTS
-        val pageNumber = 5 // 0 indexed
-        val xPercent = 1.0f
-        val yPercent = 1.0f
-        val zoomScale = 2.0f
+    fun jumpTo(page: Int, xPercent: Float, yPercent: Float, zoomScale: Float) {
 
-        testScrollAndZoomToSpecificPlace(pageNumber, xPercent, yPercent, zoomScale)
-    }
+        binding.pdfView.resetZoom() // must reset zoom BEFORE jump to page or it interrupts jumpTo. Also, we must reset zoom because if we're already zoomed in then zoomWithAnimation doesnt work lololol
+        binding.pdfView.jumpTo(page, true)
 
-    fun testScrollAndZoomToSpecificPlace(page: Int, xPercent: Float, yPercent: Float, zoomScale: Float) {
-        lifecycleScope.launch {
-
-            println("@@ Scroll & Zoom to page ${page+1}")
-
-//            var yOffsetOfPreviousPages = 0f
-//            for (x in 0 until page) {
-//                val pageSize = binding.pdfView.getPageSize(x)
-//                yOffsetOfPreviousPages += pageSize.height
-//                yOffsetOfPreviousPages += binding.pdfView.spacingPx
-//                println("@@ did add height ${pageSize.height} from page $x")
-//            }
-
-
-            // try to zoom into the exact center of some page
-//            val thisPageSize = binding.pdfView.getPageSize(page)
-//            val xOffset = xPercent * thisPageSize.width
-//            val thisYOffset = (yPercent * thisPageSize.height)
-//            val yOffset = yOffsetOfPreviousPages + thisYOffset
-//            println("@@ did add height $thisYOffset from page $page")
-//            println("@@ final yOffset: $yOffset")
-
-
-            //doesnt work
-//            binding.pdfView.zoomWithAnimation(xOffset, yOffset, zoomScale)
-
-
-            //set page offset
-            //NOTE: CANT SCROLL TO EXACT PAGE. maybe this is a lost cause
-            val offsetPerPage = 1.0 / binding.pdfView.pageCount.toDouble()                              //should be 0.010526316%
-            val positionOffset = page * offsetPerPage                                                   //should be 0.05263158 for page 6
-            binding.pdfView.positionOffset = positionOffset.toFloat()
-            println("@@ Did scroll to offset:$positionOffset with offsetPerPage: $offsetPerPage")
-
-            //zoom in
-//            val point = PointF(xPercent, yPercent)
-//            binding.pdfView.zoomCenteredRelativeTo(zoomScale, point)
-        }
+        // Queue up the zoom-in. See notes in onPageScroll
+        queuedZoom = ZoomOperation(page, xPercent, yPercent, zoomScale)
     }
 }
